7d772952ebf71c5190433d55eb525e506a765676
Alex <aironer@yandex.ru>
2024-07-25 15:43:58 +0300

    Добавление изображений к списку и их стилизация

--- components/Main.js
+++ components/Main.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { Button, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { gStyle } from "../styles/styles"
 
 export default function Main({ navigation }) {
@@ -9,11 +9,24 @@ export default function Main({ navigation }) {
       anons: "Google!!!",
       full: "Google is cool",
       key: "1",
+      img: "https://habrastorage.org/getpro/habr/upload_files/dac/c6c/3fa/dacc6c3facf61f5fc7f8015d3da7a5f8.jpg",
     },
 
-    { name: "Apple", anons: "Apple!!!", full: "Apple is cool", key: "2" },
+    {
+      name: "Apple",
+      anons: "Apple!!!",
+      full: "Apple is cool",
+      key: "2",
+      img: "https://pic.rutubelist.ru/video/c6/22/c622b7b2da40649eae281ca285e3b14b.jpg",
+    },
 
-    { name: "Facebook", Facebook: "Google!!!", full: "Google is cool", key: "3" },
+    {
+      name: "Facebook",
+      Facebook: "Google!!!",
+      full: "Google is cool",
+      key: "3",
+      img: "https://static.life.ru/posts/2016/12/948780/47550d041a46b5310f24a3359d5e637b.jpg",
+    },
   ])
 
   return (
@@ -26,6 +39,13 @@ export default function Main({ navigation }) {
         renderItem={({ item }) => (
           // item  - передаем ту статью на которую нажали
           <TouchableOpacity onPress={() => navigation.navigate("FullInfo", item)}>
+            <Image
+              source={{
+                height: 200,
+                uri: item.img,
+                width: "100%",
+              }}
+            />
             <Text>{item.name}</Text>
             <Text>{item.anons}</Text>
           </TouchableOpacity>
