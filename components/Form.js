94815b8e7fe56ee2daebd6e03d0ab9d610954ae4
Alex <aironer@yandex.ru>
2024-08-01 20:55:27 +0300

    Добавление фотографий через выбор файла 1. Установил expo-image-picker для добавления фото 2. Простейшая реализация добавления изображения по кнопке

--- components/Form.js
+++ components/Form.js
@@ -1,9 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, StyleSheet, Text, TextInput, View } from "react-native"
 import { gStyle } from "../styles/styles"
 import { Formik } from "formik"
 
+// import * as ImagePicker from "react-native-image-picker"
+// import { launchImageLibrary } from "react-native-image-picker"
+// import { PermissionsAndroid } from "react-native"
+import * as ImagePicker from "expo-image-picker"
+
 export default function Form({ addArticle, setModalWindow }) {
+  const [photos, setPhotos] = useState([])
+  const selectImage = async (props) => {
+    const result = await ImagePicker.launchImageLibraryAsync({ mediaType: "mixed", selectionLimit: 1 })
+
+    if (result.assets) {
+      console.log(result.assets)
+
+      const { uri } = result.assets[0]
+      console.log(uri)
+      // Обновляем состояние photos
+      setPhotos([{ uri }])
+
+      // Устанавливаем выбранное изображение в значения формы
+      props.setFieldValue("img", uri)
+      // сохраняем фото что были и добавляем новые
+      // setPhotos((prevState) => [...prevState, { uri: result.assets[0]?.uri }])
+      //  setPhotos([{ uri: result.assets[0]?.uri }])
+
+      if (photos.uri) {
+        console.log(photos.uri)
+        //  addArticle(photos.uri)
+      }
+    }
+  }
+
   return (
     <View style={[gStyle.main, styles.form]}>
       <Formik
@@ -42,11 +72,13 @@ export default function Form({ addArticle, setModalWindow }) {
             <TextInput
               style={styles.input}
               value={props.values.img}
-              placeholder="Добавьте фото"
+              placeholder="Ссылка на выбранный файл"
               onChangeText={props.handleChange("img")}
             />
-
-            <Button title="Добавить" onPress={props.handleSubmit} />
+            <Button title="Выбрать файл" onPress={() => selectImage(props)} />
+            <View style={styles.button}>
+              <Button title="Добавить" onPress={props.handleSubmit} />
+            </View>
           </View>
         )}
       </Formik>
@@ -64,5 +96,8 @@ const styles = StyleSheet.create({
     padding: 10,
     borderColor: "silver",
     borderRadius: 5,
+  },
+  button: {
+    marginTop: 10,
   },
 })
